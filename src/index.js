c782dfc8676cc6226d40460d9999179346a313bf
ljosberinn <admin@gerritalex.de>
2019-11-29 20:57:12 +0100

    streamlined responses, removed deadweight

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ const ERRORS = {
     'Sorry, your request could not be processed. Please try again at a later time.',
   noResults: search => `Sorry, could not find any documents for \`${search}\`.`,
   unknownError:
-    'Sorry, something went wrong. If this issue persists, please file an issue.',
+    'Sorry, something went wrong. If this issue persists, please file an issue at https://github.com/ljosberinn/discord-mdn-bot',
+};
+
+const RESPONSES = {
+  usage: 'Usage: `!mdn <search term, e.g. localStorage>`',
+  result: url => `I found this: ${url}`,
 };
 
 client.on('message', async msg => {
@@ -22,10 +27,15 @@ client.on('message', async msg => {
     return;
   }
 
-  const search = msg.content.substr(keyword.length);
+  const search = msg.content.substr(keyword.length).trim();
 
-  if (search === 'help') {
-    msg.reply(`Usage: \`!mdn <search term, e.g. localStorage>\``);
+  if (search === '<@!281576651526111232>') {
+    msg.reply('Change your name back ffs.');
+    return;
+  }
+
+  if (search.length === 0 || search === 'help') {
+    msg.reply(RESPONSES.usage);
     return;
   }
 
@@ -41,12 +51,13 @@ client.on('message', async msg => {
 
     const parser = new DOMParser();
     const document = parser.parseFromString(text);
-    const searchResults = document.getElementsByClassName('search-results')[0];
 
-    const resultMeta = searchResults.getElementsByClassName('result-meta')[0]
-      .innerHTML;
-
-    if (resultMeta.startsWith('0 documents found')) {
+    // meta gives information about the amount of results found
+    if (
+      document
+        .getElementsByClassName('result-meta')[0]
+        .textContent.startsWith('0 documents found')
+    ) {
       msg.reply(ERRORS.noResults(search));
       return;
     }
@@ -56,7 +67,7 @@ client.on('message', async msg => {
     const title = result.getElementsByClassName('result-title')[0];
     const url = buildDirectUrl(title.getAttribute('href'));
 
-    msg.reply(`I found this: ${url}`);
+    msg.reply(RESPONSES.result(url));
   } catch (error) {
     console.error(error);
     msg.reply(ERRORS.unknownError);
