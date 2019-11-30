96e7c875cfb006c8900eea6b2693ff0a041a7dd4
ljosberinn <admin@gerritalex.de>
2019-11-30 21:37:01 +0100

    lower case addrsesing

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,10 @@ const KEYWORD = '!mdn ';
 
 const ERRORS = {
   invalidResponse:
-    'Sorry, your request could not be processed. Please try again at a later time.',
-  noResults: search => `Sorry, could not find any documents for \`${search}\`.`,
+    'sorry, your request could not be processed. Please try again at a later time.',
+  noResults: search => `sorry, could not find any documents for \`${search}\`.`,
   unknownError:
-    'Sorry, something went wrong. If this issue persists, please file an issue at https://github.com/ljosberinn/discord-mdn-bot',
+    'sorry, something went wrong. If this issue persists, please file an issue at https://github.com/ljosberinn/discord-mdn-bot',
 };
 
 module.exports = {
