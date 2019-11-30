421c299edd0e299da7bfd3932784e7d0047df8e6
ljosberinn <admin@gerritalex.de>
2019-11-30 21:48:24 +0100

    follow up on breaking tagging attempts

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ client.once('ready', () => {
  * @param {Discord.Message} msg
  */
 const handleMessage = async msg => {
-  if (!msg.content.startsWith(KEYWORD) || msg.content.startsWith('`@')) {
+  if (!msg.content.startsWith(KEYWORD) || msg.content.indexOf('`@') > -1) {
     return;
   }
 
