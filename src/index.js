9af2b633044af65fd07465fdde1c9ebb18ec9694
ljosberinn <admin@gerritalex.de>
2019-11-30 22:18:52 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -22,8 +22,6 @@ client.once('ready', () => {
  * @param {Discord.Message} msg
  */
 const handleMessage = async msg => {
-  //const content = msg.content.replace(/([@`])/g, '\\$1');
-
   const content = msg.content.replace(/<@.?[0-9]*?>/g, '');
 
   if (!content.toLowerCase().startsWith(KEYWORD)) {
@@ -57,12 +55,6 @@ const handleMessage = async msg => {
     // meta provides information about the amount of results found
     const meta = document.getElementsByClassName('result-meta')[0].textContent;
     if (meta.startsWith('0 documents found')) {
-      /*msg.reply(ERRORS.noResults(search)).then(sentMessage =>
-        setTimeout(() => {
-          sentMessage.delete();
-        }, 1000 * 60),
-      );*/
-
       const sentMessage = await msg.reply(ERRORS.noResults(search));
 
       setTimeout(() => {
