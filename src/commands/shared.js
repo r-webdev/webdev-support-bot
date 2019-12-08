73ffea36be16e7be3f53690b94d6fa3d80960c62
ljosberinn <admin@gerritalex.de>
2019-12-08 20:23:30 +0100

    update general description

--- src/commands/shared.js
+++ src/commands/shared.js
@@ -3,7 +3,7 @@ const { createMarkdownLink } = require('../utils/discordTools');
 module.exports = `
 :bulb: *react with a number (:one:, :two:, ...) to filter your result*
 :neutral_face: *react with ❌ or ✖️ to delete*
-:point_up: *supports npm and mdn, caniuse & composer upcoming*
+:point_up: *supports !npm, !composer, !mdn*
 :gear: *issues? feature requests? head over to ${createMarkdownLink(
   'github',
   process.env.REPO_LINK,
