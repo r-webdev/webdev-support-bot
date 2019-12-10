bbb7b31efce31e33cb90ae36aa3f5a17eace3922
ljosberinn <admin@gerritalex.de>
2019-12-10 16:39:33 +0100

    move BASE_DESCRIPTION, add createDescription fn

--- src/utils/discordTools.js
+++ src/utils/discordTools.js
@@ -10,6 +10,15 @@ const { Message } = require('discord.js');
 const createMarkdownLink = (title, url) =>
   `[${title}](${url.replace(/\)/g, '\\)')})`;
 
+const BASE_DESCRIPTION = `
+:bulb: *react with a number (:one:, :two:, ...) to filter your result*
+:neutral_face: *react with ❌ or ✖️ to delete*
+:point_up: *supports !npm, !composer, !mdn and !caniuse*
+:gear: *issues? feature requests? head over to ${createMarkdownLink(
+  'github',
+  process.env.REPO_LINK,
+)}*`;
+
 /**
  *
  * @param {string} provider
@@ -138,10 +147,25 @@ const adjustDescriptionLength = (position, name, description) => {
   return description;
 };
 
+/**
+ *
+ * @param {number} index
+ * @param {string} content
+ */
+const createMarkdownListItem = (index, content) => `${index + 1}. ${content}`;
+
+/**
+ *
+ * @param {array} items
+ */
+const createDescription = items => items.concat(BASE_DESCRIPTION).join('\n');
+
 module.exports = {
   createMarkdownLink,
   createListEmbed,
   createEmbed,
   delayedAutoDeleteMessage,
   adjustDescriptionLength,
+  createMarkdownListItem,
+  createDescription,
 };
