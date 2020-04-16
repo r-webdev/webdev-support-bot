a6edc1f05af17b4749e46f47e5326ae6d0a9025c
ljosberinn <admin@gerritalex.de>
2020-04-16 18:14:47 +0200

    try catch modChannel message sending

--- src/spam_filter/handler.js
+++ src/spam_filter/handler.js
@@ -1,13 +1,9 @@
 const { createEmbed, createMarkdownBash } = require('../utils/discordTools');
+const errors = require('../utils/errors');
 
-module.exports = ({
-  userID,
-  username,
-  discriminator,
-  channel,
-  msgID,
-  server,
-}) => {
+module.exports = async (msg) => {
+  const { userID, username, discriminator, channel, msgID, server } = msg;
+
   const modChannel = server.channels.cache.find(
     ({ name }) => name === process.env.MOD_CHANNEL
   );
@@ -19,25 +15,30 @@ module.exports = ({
   const url = `https://discordapp.com/channels/${server.id}/${channel.id}/${msgID}`;
   const user = `@${username}#${discriminator}`;
 
-  modChannel.send(
-    createEmbed({
-      provider: 'spam',
-      description: 'Spam has been detected on the server.',
-      author: userID,
-      url,
-      title: 'Alert!',
-      fields: [
-        { name: 'User', value: user, inline: true },
-        { name: 'Channel', value: channel, inline: true },
-        {
-          name: 'Command',
-          value: createMarkdownBash(
-            `?mute ${user} 5h Spamming in #${channel.name}`
-          ),
-        },
-        { name: 'Message Link', value: url },
-      ],
-      footerText: 'Spam Filter',
-    })
-  );
+  try {
+    await modChannel.send(
+      createEmbed({
+        provider: 'spam',
+        description: 'Spam has been detected on the server.',
+        author: userID,
+        url,
+        title: 'Alert!',
+        fields: [
+          { name: 'User', value: user, inline: true },
+          { name: 'Channel', value: channel, inline: true },
+          {
+            name: 'Command',
+            value: createMarkdownBash(
+              `?mute ${user} 5h Spamming in #${channel.name}`
+            ),
+          },
+          { name: 'Message Link', value: url },
+        ],
+        footerText: 'Spam Filter',
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    await msg.reply(errors.unknownError);
+  }
 };
