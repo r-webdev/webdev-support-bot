f9d1533cfaee462b222d19f0ac0763baecb359c1
ljosberinn <admin@gerritalex.de>
2020-04-18 00:56:34 +0200

    feat(cmd): add additional sites to !code

--- src/commands/code/index.ts
+++ src/commands/code/index.ts
@@ -4,6 +4,8 @@ import { warning } from '../../utils/emojis';
 const sites = [
   `https://codepen.io/ - ${warning} requires account`,
   'https://codesandbox.io/',
+  'https://repl.it/',
+  'https://ideone.com/',
 ].join('\n');
 
 const handleCodeRequest = async (msg: Message) => {
