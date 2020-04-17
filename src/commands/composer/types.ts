d9bba25e6aa381ac3521a50e72a6694cb01bbccf
ljosberinn <admin@gerritalex.de>
2020-04-17 22:44:00 +0200

    feat(migration): composer

---
+++ src/commands/composer/types.ts
@@ -0,0 +1,68 @@
+export interface PackagistResponse {
+  results: {
+    name: string;
+    description: string;
+    url: string;
+    repository: string;
+    downloads: number;
+    favers: number;
+  }[];
+  total: number;
+  next?: string;
+}
+
+export interface ExtendedPackagistResponse {
+  package: {
+    name: string;
+    description: string;
+    time: Date;
+    maintainers: {
+      name: string;
+      avatar_url: string;
+    }[];
+    versions: Versions;
+    type: string;
+    repository: string;
+    github_stars: number;
+    github_watchers: number;
+    github_forks: number;
+    github_open_issues: number;
+    language: string;
+    dependents: number;
+    suggesters: number;
+    downloads: Downloads;
+    favers: number;
+  };
+}
+
+export interface Versions {
+  'dev-master': Version;
+}
+
+export interface Version {
+  name: string;
+  description: string;
+  keywords: string[];
+  homepage: string;
+  version: string;
+  version_normalized: string;
+  license: string[];
+  funding: boolean;
+  time: Date;
+  source: {
+    type: string;
+    url: string;
+    reference: string;
+  };
+  require: { [key: string]: string };
+  authors: {
+    name: string;
+    email: string;
+  }[];
+}
+
+export interface Downloads {
+  total: number;
+  monthly: number;
+  daily: number;
+}
