99b012c3e2321a494b221938beeeacf2a983881f
ljosberinn <admin@gerritalex.de>
2020-04-18 00:12:26 +0200

    feat(cmd): add more trivia fns

--- src/commands/formatting/index.ts
+++ src/commands/formatting/index.ts
@@ -32,7 +32,33 @@ function typeOfNestor() {
   return 'naab';
 }
 
-const exampleFns = [formatFn, annoyTitan, getActualName, typeOfNestor];
+function getTheBestOfSteves() {
+  return 'DLSteve';
+}
+
+const typeMap = {};
+
+function getEstimatedPayment(type, rate, hours) {
+  if (type === 'equity') {
+    return 0;
+  }
+
+  return rate * hours * typeMap[type];
+}
+
+function getBenzFavoriteWord() {
+  return 'oop';
+}
+
+const exampleFns = [
+  formatFn,
+  annoyTitan,
+  getActualName,
+  typeOfNestor,
+  getTheBestOfSteves,
+  getBenzFavoriteWord,
+  getEstimatedPayment,
+];
 
 const getSnippetElements = () =>
   [
