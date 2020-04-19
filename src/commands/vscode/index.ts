698d054843a89af82a5184f08be3adf1b92fc9f6
ljosberinn <admin@gerritalex.de>
2020-04-20 00:55:51 +0200

    fix(cmd): tweak vsCodeIcon binding

--- src/commands/vscode/index.ts
+++ src/commands/vscode/index.ts
@@ -1,16 +1,23 @@
-import { Message } from 'discord.js';
+import { Message, GuildEmoji } from 'discord.js';
 import { light } from '../../utils/emojis';
 
-const message = `
-${light} consider using a lightweight, customizeable and monthly updated editor such as
-
-:vscode: Visual Studio Code - <https://code.visualstudio.com/>
-
-It's free & available cross platform and next to WebStorm the go to editor in this industry.
-`;
+const createMessage = (vsCodeIcon: GuildEmoji) =>
+  [
+    `> ${light} consider using a lightweight, customizeable and monthly updated editor such as`,
+    '> ',
+    vsCodeIcon
+      ? `> ${vsCodeIcon} Visual Studio Code - <https://code.visualstudio.com/>`
+      : '> Visual Studio Code - <https://code.visualstudio.com/>',
+    '> ',
+    "> It's free & available cross platform and next to WebStorm the go to editor in this industry.",
+  ].join('\n');
 
 const handleVSCodeRequest = async (msg: Message) => {
-  await msg.channel.send(message);
+  const vsCodeIcon = msg.client.emojis.cache.find(
+    (emoji) => emoji.name === 'vscode'
+  );
+
+  await msg.channel.send(createMessage(vsCodeIcon));
 };
 
 export default handleVSCodeRequest;
