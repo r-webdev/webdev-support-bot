f2f97d49fa04e2542fd1b23e9e206cebc493d06e
Denis Onder <denis_onder@protonmail.com>
2020-06-14 12:26:19 +0200

    Storing the user and storing points

--- src/helpful_role/db_model.ts
+++ src/helpful_role/db_model.ts
@@ -8,7 +8,7 @@ const schema = new Schema({
   },
   user: {
     required: true,
-    type: Number,
+    type: String,
   },
 });
 
--- src/helpful_role/index.ts
+++ src/helpful_role/index.ts
@@ -1,8 +1,13 @@
 import { Message } from 'discord.js';
-
 import { HELPFUL_ROLE_ID } from '../env';
+import { Document } from 'mongoose';
+import HelpfulRoleMemberModel from './db_model';
 
-export default (message: Message) => {
+interface IUser extends Document {
+  points?: number;
+}
+
+export default async (message: Message) => {
   // Check if the message author has the helpful role
   const isHelpfulRoleMember = message.member.roles.cache.find(
     r => r.id === HELPFUL_ROLE_ID
@@ -10,4 +15,20 @@ export default (message: Message) => {
   if (!isHelpfulRoleMember) return;
 
   // Find or create a database entry
+  let user: IUser = await HelpfulRoleMemberModel.findOne({
+    user: message.member.id,
+  });
+  if (!user)
+    user = await HelpfulRoleMemberModel.create({
+      user: message.member.id,
+    });
+
+  // Add a point to the user
+  user.points++;
+
+  // Save the user
+  user
+    .save()
+    .then(updated => console.log(`${updated.id} => ${updated.points}`))
+    .catch(err => console.error('user.save(): ', err));
 };
