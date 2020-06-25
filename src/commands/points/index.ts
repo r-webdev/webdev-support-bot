b9a85bb338535e608bb81714bef590037aded82c
Denis Onder <denis_onder@protonmail.com>
2020-06-25 21:27:11 +0200

    Optional URLs in embed generator, embeds for point outputs

--- src/commands/points/index.ts
+++ src/commands/points/index.ts
@@ -2,12 +2,24 @@ import { Message } from 'discord.js';
 
 import { IUser } from '../../helpful_role';
 import User from '../../helpful_role/db_model';
+import { createEmbed } from '../../utils/discordTools';
 
 export default async (msg: Message) => {
   try {
-    const user: IUser = await User.findOne({ user: msg.author.id });
-    msg.reply(user.points ? user.points : 0);
+    const { points }: IUser = await User.findOne({ user: msg.author.id });
+    const output = createEmbed({
+      description: `You have accumulated ${points ? points : 0} point${
+        points === 1 ? '' : 's'
+      }.`,
+      footerText: 'Helpful User Points',
+      provider: 'spam',
+      title: msg.author.tag,
+    });
+
+    msg.channel.send(output);
   } catch (error) {
     console.error('catch -> points/index.ts:', error);
+  } finally {
+    msg.delete();
   }
 };
