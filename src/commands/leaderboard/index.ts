48347f5e33d4af9773e94fb7e2b27d64c342063c
Denis Onder <denis_onder@protonmail.com>
2020-06-25 18:04:35 +0200

    Printing out the leaderboard in an embed

--- src/commands/leaderboard/index.ts
+++ src/commands/leaderboard/index.ts
@@ -2,15 +2,33 @@ import { Message } from 'discord.js';
 
 import { IUser } from '../../helpful_role';
 import User from '../../helpful_role/db_model';
+import { createEmbed } from '../../utils/discordTools';
+import { OutputField } from '../post';
 
 export default async (msg: Message) => {
   try {
-    const topUsers = [...(await User.find().sort({ points: -1 }).limit(10))];
-    const output = topUsers.reduce(
-      (acc, curr: IUser, i) =>
-        (acc += `${i + 1}. <@!${curr.user}> -> ${curr.points}\n`),
-      ''
+    const topUsers: IUser[] = [
+      ...(await User.find().sort({ points: -1 }).limit(10)),
+    ];
+
+    const fields: OutputField[] = topUsers.map(
+      ({ user, points }, i): OutputField => {
+        return {
+          inline: false,
+          name: `${i + 1}.`,
+          value: `<@!${user}>: ${points} point${points === 1 ? 's' : ''}.`,
+        };
+      }
     );
+
+    const output = createEmbed({
+      description: 'Top helpful users:',
+      fields,
+      footerText: 'Leaderboard: Helpful Users',
+      provider: 'spam',
+      title: 'Leaderboard: Helpful Users',
+      url: 'https://discord.com/',
+    });
 
     msg.channel.send(output);
   } catch (error) {
