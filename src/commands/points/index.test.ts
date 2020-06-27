0342c5f4c045bed997edab865ac89301b1b3ac58
Denis Onder <denis_onder@protonmail.com>
2020-06-27 18:26:36 +0200

    Points command test implemented

--- src/commands/points/index.test.ts
+++ src/commands/points/index.test.ts
@@ -10,9 +10,9 @@ test('should throw an error if no user has been found', async () => {
     author: { id: '1', tag: 'test#1234' },
     channel: { send: jest.fn() },
     delete: jest.fn(),
-    reply: jest.fn(),
   };
 
   await pointsHandler(msg);
+
   expect(msg.channel.send.mock.calls[0]).toMatchSnapshot();
 });
