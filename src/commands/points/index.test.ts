a35f8d2a9e92b040b32fcd3d4ac9d395c0ce2235
Denis Onder <denis_onder@protonmail.com>
2020-06-27 10:37:34 +0200

    Simple test for points command, changed jest test env to be compliant with node

---
+++ src/commands/points/index.test.ts
@@ -0,0 +1,17 @@
+import { userNotFound } from '../../utils/errors';
+
+import pointsHandler from '.';
+
+test('should throw an error if no user has been found', async () => {
+  const sendMock = jest.fn();
+  const replyMock = jest.fn();
+
+  const msg: any = {
+    author: { id: '1' },
+    channel: { send: sendMock },
+    reply: replyMock,
+  };
+
+  const res = await pointsHandler(msg);
+  expect(res).toThrowError(userNotFound);
+});
