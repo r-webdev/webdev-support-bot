2b1a71e5412216742adfe23b3b030378252e433e
Denis Onder <denis_onder@protonmail.com>
2020-06-27 10:41:46 +0200

    Error for not finding a user, points test

--- src/commands/points/index.test.ts
+++ src/commands/points/index.test.ts
@@ -7,7 +7,7 @@ test('should throw an error if no user has been found', async () => {
   const replyMock = jest.fn();
 
   const msg: any = {
-    author: { id: '1' },
+    author: { id: '1', tag: 'test#1234' },
     channel: { send: sendMock },
     reply: replyMock,
   };
