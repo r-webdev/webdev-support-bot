523275733f0784ac53ac1d7148cd1e90c45a31da
Denis Onder <denis_onder@protonmail.com>
2020-06-28 19:28:23 +0200

    Decoupling the handler for givig out points from the initial helpful role script

--- src/helpful_role/index.ts
+++ src/helpful_role/index.ts
@@ -2,7 +2,7 @@ import { MessageReaction } from 'discord.js';
 import { Document } from 'mongoose';
 
 import { HELPFUL_ROLE_ID } from '../env';
-import HelpfulRoleMemberModel from './db_model';
+import pointHandler from './point_handler';
 
 export interface IUser extends Document {
   user?: string;
@@ -11,7 +11,8 @@ export interface IUser extends Document {
 
 export default async (reaction: MessageReaction) => {
   // Break if the author of the message is trying to upvote his own solution
-  if (reaction.users.cache.find(u => u.id === reaction.message.id)) return;
+  if (reaction.users.cache.find(u => u.id === reaction.message.author.id))
+    return;
 
   // Check if the message author has the helpful role
   const isHelpfulRoleMember = reaction.message.member.roles.cache.find(
@@ -19,21 +20,6 @@ export default async (reaction: MessageReaction) => {
   );
   if (!isHelpfulRoleMember) return; // Handle the case if the user does not have the helpful role
 
-  // Find or create a database entry
-  let user: IUser = await HelpfulRoleMemberModel.findOne({
-    user: reaction.message.member.id,
-  });
-  if (!user)
-    user = await HelpfulRoleMemberModel.create({
-      user: reaction.message.member.id,
-    });
-
-  // Add a point to the user
-  user.points++;
-
-  // Save the user
-  user
-    .save()
-    .then(updated => console.log(`${updated.id} => ${updated.points}`))
-    .catch(error => console.error('user.save():', error));
+  // Give the user a point
+  await pointHandler(reaction.message.member.id);
 };
