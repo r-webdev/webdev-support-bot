c633fba00484af25e4c51fb3cbdbc766d799bdbe
Denis Onder <denis_onder@protonmail.com>
2020-06-30 08:14:32 +0200

    Setting up a notifier for when the user has been granted the role

--- src/helpful_role/point_handler.ts
+++ src/helpful_role/point_handler.ts
@@ -1,32 +1,33 @@
-import { Guild } from 'discord.js';
+import { Message } from 'discord.js';
 
 import { HELPFUL_ROLE_ID } from '../env';
+import { createEmbed } from '../utils/discordTools';
 import HelpfulRoleMember from './db_model';
 
 import { IUser } from '.';
 
-const result = (granted: boolean, message: string) => ({ granted, message });
-
-const grantHelpfulRole = async (userID: string, guild: Guild) => {
-  const user = guild.members.cache.find(u => u.id === userID);
-  if (!user) return result(false, 'User does not exist.');
+const grantHelpfulRole = async (userID: string, msg: Message) => {
+  const user = msg.guild.members.cache.find(u => u.id === userID);
+  if (!user) return;
 
   // Check if the user has the role
-  if (user.roles.cache.find(r => r.id === HELPFUL_ROLE_ID))
-    return result(
-      false,
-      `<@!${userID}> already has the <@&${HELPFUL_ROLE_ID}> role.`
-    );
+  if (user.roles.cache.find(r => r.id === HELPFUL_ROLE_ID)) return;
 
   // Add the role to the user
   await user.roles.add(HELPFUL_ROLE_ID);
-  return result(
-    true,
-    `<@!${userID}> has been granted the <@&${HELPFUL_ROLE_ID}> role!`
+
+  // Send notification message
+  msg.channel.send(
+    createEmbed({
+      description: `<@!${userID}> has been granted the <@&${HELPFUL_ROLE_ID}> role!`,
+      footerText: 'Helpful Role Handler',
+      provider: 'spam',
+      title: 'A user has received the Helpful role!',
+    })
   );
 };
 
-export default async (userID: string, guild: Guild) => {
+export default async (userID: string, msg: Message) => {
   let user: IUser = await HelpfulRoleMember.findOne({
     user: userID,
   });
@@ -39,7 +40,7 @@ export default async (userID: string, guild: Guild) => {
   user.points++;
 
   // Check if the user has enough points to be given the helpful role
-  if (user.points >= 10) grantHelpfulRole(userID, guild);
+  if (user.points >= 10) await grantHelpfulRole(userID, msg);
 
   // Save the user
   user
