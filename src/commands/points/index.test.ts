3a001fe80aaeaf0629f820c33192cb2cf92e2ca0
Denis Onder <denis_onder@protonmail.com>
2020-06-30 11:05:42 +0200

    Fixed the failing test

--- src/commands/points/index.test.ts
+++ src/commands/points/index.test.ts
@@ -8,6 +8,7 @@ test('should throw an error if no user has been found', async () => {
   const msg: any = {
     author: { id: '1', tag: 'test#1234' },
     channel: { send: jest.fn() },
+    content: '!points',
     delete: jest.fn(),
   };
 
