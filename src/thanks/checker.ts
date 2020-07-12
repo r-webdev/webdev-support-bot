92d0ebfac3e611ae150fe986f7eaba299b0048e3
Denis Onder <denis_onder@protonmail.com>
2020-07-12 18:24:27 +0200

    Changing up the thanking validation yet again

--- src/thanks/checker.ts
+++ src/thanks/checker.ts
@@ -1,9 +1,9 @@
-const abbreviations = /^(ty)|(thanks)|(thx)|(tnx)|(tyvm)|(thank)$/g;
+const abbreviations = new Set(['ty', 'thanks', 'thx', 'tnx', 'tyvm', 'thank']);
 
 const keywordValidator = (msg: string) => {
   const fragments = msg.split(' ');
 
-  return fragments.some(fragment => abbreviations.test(fragment));
+  return fragments.some(fragment => abbreviations.has(fragment));
 };
 
 export default keywordValidator;
