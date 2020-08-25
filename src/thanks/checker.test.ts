1bf99ee3efeb3d9e09ccaedb25195abb5f282cca
Gerrit Alex <admin@gerritalex.de>
2020-08-26 01:14:10 +0200

    chore: deps
    - fix versions
    - remove dependabot
    - add renovatebot

--- src/thanks/checker.test.ts
+++ src/thanks/checker.test.ts
@@ -1,25 +1,16 @@
 import checker from './checker';
 
 describe('thanks checked', () => {
-  test('returns false if thanks not in message', () => {
-    expect(checker('naab')).toBe(false);
-  });
-
-  test('returns true if thanks in message', () => {
-    expect(checker('thanks friend')).toBe(true);
-    expect(checker('ty friend')).toBe(true);
-  });
-
-  test('returns true if thanks in message, case insensitive', () => {
-    expect(checker('tHaNKs fRiEnDO')).toBe(true);
-    expect(checker('tY fRiEnDO')).toBe(true);
-  });
-
-  test('returns false if ty is part of a word', () => {
-    expect(checker('time for a putty party')).toBe(false);
-  });
-
-  test('returns true if ty is followed or preceded by punctuation', () => {
-    expect(checker('"ty, friend"')).toBe(true);
+  test.each([
+    ['naab', false],
+    ['thanks friend', true],
+    ['Thanks friend', true],
+    ['ty friend', true],
+    ['tHaNKs fRiEnDO', true],
+    ['tY fRiEnDO', true],
+    ['time for a putty party', false],
+    ['"ty, friend"', true],
+  ])('acts only on appropriate input', (string, result) => {
+    expect(checker(string)).toBe(result);
   });
 });
