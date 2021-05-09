58e105685ff2950820b20082517c22c7a6bf3d73
Adam D <adam.x.dorling@gmail.com>
2021-05-09 15:14:36 +0100

    🔥 Remove sandbox thumnail (#608)
    * Update index.test.ts.snap
    
    * remove thumbnail

--- src/commands/code/index.ts
+++ src/commands/code/index.ts
@@ -4,7 +4,7 @@ import { warning } from '../../utils/emojis';
 
 const sites = [
   `<https://codepen.io/> - ${warning} requires account`,
-  'https://codesandbox.io/',
+  '<https://codesandbox.io/>',
   '<https://repl.it/>',
   '<https://ideone.com/>',
   '<https://jsfiddle.net/>',
