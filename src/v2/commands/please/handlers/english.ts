aec8779ea462e0f20bf63482b45d7394eb02c7bb
찰스 <osairis100@gmail.com>
2023-06-16 20:46:09 +0800

    fix: korean phrase in please command (#955)

--- src/v2/commands/please/handlers/english.ts
+++ src/v2/commands/please/handlers/english.ts
@@ -1,6 +1,6 @@
 export const english = [
   'english',
-  `English only please - انجليزي فقط من فضلك - Само на английски - 请只说英语 - Pouze v angličtině - Kun engelsk takk - Angla Nur Bonvolu - Vain englanti - Anglais seulement s'il vous plait - Bitte nur Englisch - Μόνο Αγγλικά - Hanya Bahasa Inggris - Solo inglese per favore - 英語のみ - 영어 만주세요 - Tikai angļu valodā, lūdzu - Tik angliškai Prašau - Solo inglés por favor - Пожалуйста, только на английском - Iba v angličtine - Sadece ingilizce lütfen - Тільки англійською мовою - Proszę tylko po angielsku
+  `English only please - انجليزي فقط من فضلك - Само на английски - 请只说英语 - Pouze v angličtině - Kun engelsk takk - Angla Nur Bonvolu - Vain englanti - Anglais seulement s'il vous plait - Bitte nur Englisch - Μόνο Αγγλικά - Hanya Bahasa Inggris - Solo inglese per favore - 英語のみ - 영어로만 해주세요 - Tikai angļu valodā, lūdzu - Tik angliškai Prašau - Solo inglés por favor - Пожалуйста, только на английском - Iba v angličtine - Sadece ingilizce lütfen - Тільки англійською мовою - Proszę tylko po angielsku
 
 This allows us to effectively moderate and gives everyone an opportunity to join in on the conversation.`,
 ] as const;
